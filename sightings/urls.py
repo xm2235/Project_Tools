b79f418ee9a5bb707b35445449c3f36dde813ff1
Ziyang Zhang <zz2732@columbia.edu>
2020-05-12 08:09:17 +0000

    Add sightings template folder and views in urls.py.

--- sightings/urls.py
+++ sightings/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index),
-
+    path('', views.index),
+    path('<uique_squirrel_id>', views.update),
+    path('add', views.add),
+    path('stats/', views.get_stats),
 
 ]
 
--- sightings/views.py
+++ sightings/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Sighting
+
 def index(request):
     return HttpResponse('Project Squirrel')
-
-
 
 
