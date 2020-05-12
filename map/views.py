ddab56cc5df0feae9b88f8b3251615dd6787edf0
Ziyang Zhang <zz2732@columbia.edu>
2020-05-12 07:37:51 +0000

    More name changes.

--- map/views.py
+++ map/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from sightings.models import Sighting
 
 def index(request):
-    all_sightings = Squirrel.objects.all()[:100]
+    all_sightings = Sighting.objects.all()[:100]
     context = {'all_sightings': all_sightings}
     return render(request, 'map/map.html', context)
 
