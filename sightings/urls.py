05b25c393b573368f6df5214702cd9e063668721
Ziyang Zhang <zz2732@columbia.edu>
2020-05-13 05:38:06 +0000

    Delete 'ulr.py'.

--- sightings/urls.py
+++ sightings/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('<unique_squirrel_id>/', views.unique),
-#    path('add/', views.add),
     path('stats/', views.stats),
 
 ]
