38944fd931fd490d9f5f5774479c3c2823e76340
Ziyang Zhang <zz2732@columbia.edu>
2020-05-13 08:13:11 +0000

    Switch orders in sightings/urls.py.

--- sightings/urls.py
+++ sightings/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('<unique_squirrel_id>/', views.unique),
     path('stats/', views.stats),
+    path('<unique_squirrel_id>/', views.unique),
 
 ]
 
